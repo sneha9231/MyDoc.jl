eb7fd792fa39c4ac5ad8695e4d1c806517555678
Sneha Pandey <gd6601692@gmail.com>
2023-12-23 21:34:54 +0530

    Update MyDoc.jl

--- src/MyDoc.jl
+++ src/MyDoc.jl
@@ -1,5 +1,12 @@
 module MyDoc
 
 # Write your package code here.
+# Canonical Triplets and Component Sets
 
+## Canonical Triplets
+A **canonical triplet** $\mathscr{C}$ [`AMFMtriplet`] is parameterized by an **instantaneous amplitude (IA)**
+$a(t)$ [`Function`], an **instantaneous frequency (IF)** $\omega(t)$ [`Function`], and
+a **phase reference** $\phi$ [`Real`].
+
+$\mathscr{C}\triangleq\left\{a(t),\omega(t), \phi\vphantom{0^0}\right\}$
 end
